1599223f13ff4e1d30ff1b6228bebf40f6466048
Manuel Duarte <carlosd99m@gmail.com>
2024-12-26 15:08:48 -0600

    chore: update dependencies, remove unused ApiKeyWidget, and refactor app structure

--- vehicle_recognition/lib/main.dart
+++ vehicle_recognition/lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
   runApp(const MainApp());
 }
